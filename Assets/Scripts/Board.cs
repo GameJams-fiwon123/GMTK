c7a15eab61472d7c8ac6aed1a8b8e664b583b7ad
fiwon123 <felipeinoue@yahoo.com.br>
2020-07-05 23:05:38 -0300

    Base terminada

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Board : MonoBehaviour
+{
+    AudioSource audio;
+    SpriteRenderer sprRenderer;
+    BoxCollider2D boxCol;
+    int left = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        sprRenderer = GetComponent<SpriteRenderer>();
+        boxCol = GetComponent<BoxCollider2D>();
+        SearchPieces();
+    }
+
+    private void SearchPieces()
+    {
+        Piece[] pieces = FindObjectsOfType<Piece>();
+        foreach(Piece p in pieces){
+            if (p.isCorrect)
+                left++;
+        }
+    }
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Piece>().isCorrect){
+            left--;
+            Debug.Log("Acertou");
+            audio.Play();
+            Destroy(other.gameObject);
+
+            if (left == 0){
+                sprRenderer.enabled = true;
+                boxCol.enabled = false;
+                GameManager.instance.Won();
+            }
+        } else {
+            // Nothing
+        }
+    }
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Piece : MonoBehaviour
+{
+    AudioSource audio;
+    BoxCollider2D boxCol;
+    public bool isCorrect = false;
+
+    bool holding = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        boxCol = GetComponent<BoxCollider2D>();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if (holding){
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        transform.position = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+
+
+    /// <summary>
+    /// OnMouseDown is called when the user has pressed the mouse button while
+    /// over the GUIElement or Collider.
+    /// </summary>
+    void OnMouseDown()
+    {
+       holding = true;
+       boxCol.enabled = false;
+    }
+
+    void OnMouseUp() {
+        holding = false;
+        boxCol.enabled = true;
+    }
+
+    /// <summary>
+    /// OnMouseDown is called when the user has pressed the mouse button while
+    /// over the GUIElement or Collider.
+    /// </summary>
+    void OnMouseEnter()
+    {
+        Debug.Log("Play Audio");
+        audio.Play();
+    }
+
+    void OnMouseExit() {
+        Debug.Log("Stop Audio");
+        audio.Stop();
+    }
+}
